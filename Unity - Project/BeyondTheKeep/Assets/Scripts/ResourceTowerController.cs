ff7a8cb39febf94782fd4c7999b2e7dca6c5bd54
TylerDinardo <dtyler9@dhcp-137-163.cu-wireless-students.carleton.ca>
2014-06-07 17:25:37 -0400

    Resource Tower
    Added Resource Tower prefab with functional script and placeholder art
    asset

---
+++ Unity - Project/BeyondTheKeep/Assets/Scripts/ResourceTowerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceTowerController : MonoBehaviour {
+	private int team;
+	private int bufferCount; 
+	public GameController gameController;
+	// Use this for initialization
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+
+		if (transform.position.x > 0)
+						team = 1;
+				else
+						team = 2;
+		bufferCount = 60;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bufferCount == 60) {
+			bufferCount = 0;
+			if ( team == 2 ){
+				gameController.team1Cash += 5;
+			} else {
+				gameController.team2Cash += 5;
+			}
+		} else {
+			bufferCount++;
+		}
+	}
+}
